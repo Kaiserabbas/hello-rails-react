b4bd43c876b64be73ae2e6b5d4e4a73496cc118c
Qaisar <kayser.abbas@gmail.com>
2023-12-12 14:43:19 +0500

    Add the static home.html,erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/api/v1/messages', to: 'api/v1/messages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
