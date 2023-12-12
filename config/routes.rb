e24624431b713db6b9dae5806b4a285f2cdd484d
Qaisar <kayser.abbas@gmail.com>
2023-12-12 19:03:21 +0500

    Update the roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'root#home'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/") 
-  # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index] do
+        collection do
+          get 'new_greeting'
+        end
+      end
+    end
+  end
 end
