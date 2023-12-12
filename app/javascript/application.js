8d869b9d194cf8c1f61a4a27e2689cde23689c36
Qaisar <kayser.abbas@gmail.com>
2023-12-12 19:04:06 +0500

    Updates application.js for React

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,19 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
-import { render } from 'react-dom';
+import { render, ReactDOM } from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 import App from './packs/App';
+import store from './packs/redux/store';
 
-document.addEventListener('DOMContentLoaded', () => {
-  render(
-    <App />,
-    document.body.appendChild(document.createElement('div')),
-  )
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    root
+);
