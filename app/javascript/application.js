394f10357ed1afccc15445cfdfcefea50701e61e
Qaisar <kayser.abbas@gmail.com>
2023-12-12 14:57:47 +0500

    Add Component Greeting.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import { render } from 'react-dom';
+import App from './packs/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <App />,
+    document.body.appendChild(document.createElement('div')),
+  )
+});
---
+++ app/javascript/packs/components/Greeting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Greeting = () => (
+  <div>
+    <h1>Greeting will go here</h1>
+  </div>
+);
+
+export default Greeting;
