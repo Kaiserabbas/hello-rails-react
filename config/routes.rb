78943bcd1c339b23925fb8906a93205307f3e641
Qaisar <kayser.abbas@gmail.com>
2023-12-12 14:40:34 +0500

    Add root 'static_pages#home'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get '/api/v1/messages', to: 'api/v1/messages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,6 +7,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
+  # Defines the root path route ("/") 
   # root "posts#index"
 end
