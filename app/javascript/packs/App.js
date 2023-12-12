379659e0190d8febdff71709d9aee6a64958ad6d
Qaisar <kayser.abbas@gmail.com>
2023-12-12 19:03:47 +0500

    Add react App.js

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Greeting from './Greeting';
+import Greeting from './components/Greeting';
 
 const App = () => {
   return 
