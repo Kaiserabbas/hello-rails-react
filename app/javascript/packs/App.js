3b6aea456801f89c40c08521533a3a6ea3500016
Qaisar <kayser.abbas@gmail.com>
2023-12-12 14:54:17 +0500

    Update App.js

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Greeting from './Greeting';
 
-const App = () => (
-  <Router>
-    <Route path="/" exact component={Greeting} />
-  </Router>
-);
+const App = () => {
+  return 
+  <div>
+    <Router>
+        <Route path="/" exact component={Greeting} />
+    </Router>
+  </div>
+};
 
 export default App;
